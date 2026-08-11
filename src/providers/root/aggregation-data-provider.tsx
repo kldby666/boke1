5a18b70cacbb49bfdc65e14df0cf3357a231fe6f
kldby666 <1012956003@qq.com>
2026-08-11 13:33:32 +0800

    Update aggregation-data-provider.tsx

--- src/providers/root/aggregation-data-provider.tsx
+++ src/providers/root/aggregation-data-provider.tsx
@@ -21,6 +21,17 @@ export const AggregationProvider: FC<
   }>
 > = ({ children, aggregationData, appConfig }) => {
   useBeforeMounted(() => {
+
+    if (aggregationData && !aggregationData.config) {
+    const d = aggregationData as any
+    d.config = {
+      title: d.seo?.title,
+      description: d.seo?.description,
+      server_url: d.url?.server_url,
+      web_url: d.url?.web_url,
+    }
+  }
+    
     if (!aggregationData) return
     jotaiStore.set(aggregationDataAtom, aggregationData)
     setWebUrl(aggregationData.url.webUrl)
